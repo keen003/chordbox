aaf6530eb0812cd742c81f3978b28b35db9585ea
Astrov003 <vpopovic003@gmail.com>
2022-02-17 16:28:06 +0100

    version 1.0

--- app.js
+++ app.js
@@ -1,74 +1,46 @@
 
 function switchButton(button_num)
 {
-    var container = document.getElementById("container");
-    var button_pos = container.getElementsByTagName("img");
-    var button_opc = document.getElementById("button"+button_num);
+    let button_img = document.getElementById("button_img"+button_num);
+    let button_opc = document.getElementById("button"+button_num);
+        console.log("button"+button_num);
+        if (button_img.src.match('images/hover_button.png'))
+        {
+            button_img.src = "images/button1.png";
+            button_opc.style.opacity = "1";
+        }
 
-    if (button_pos[button_num].src.match('images/hover_button.png'))
-    {
-        button_pos[button_num].src = "images/button1.png";
-        button_opc.style.opacity = "1";
-    }
-    else if (button_pos[button_num].src.match('images/button1.png'))
-    {
-        button_pos[button_num].src = "images/button2.png";
-        button_opc.style.opacity = "1";
-    }
-    else if (button_pos[button_num].src.match('images/button2.png'))
-    {
-        button_pos[button_num].src = "images/button3.png";
-        button_opc.style.opacity = "1";
-    }
-    else if (button_pos[button_num].src.match('images/button3.png'))
-    {
-        button_pos[button_num].src = "images/button4.png";
-        button_opc.style.opacity = "1";
-    }
-    else if (button_pos[button_num].src.match('images/button4.png'))
-    {
-        button_pos[button_num].src = "images/hover_button.png";
-        button_opc.style.opacity = "";
-    }
+        else if (button_img.src.match('images/button1.png'))
+            button_img.src = "images/button2.png";
+        else if (button_img.src.match('images/button2.png'))
+            button_img.src = "images/button3.png";
+        else if (button_img.src.match('images/button3.png'))
+            button_img.src = "images/button4.png";
+        else if (button_img.src.match('images/button4.png'))
+        {
+            button_img.src = "images/hover_button.png";
+            button_opc.style.opacity = "";
+        }
+
 }
 
-function resetFret() {
-    var container = document.getElementById("container");
-    var buttons = container.getElementsByTagName("img");
-    for (let i=0; i<24; i++)
+function resetFret()
+{
+    let container = document.getElementById("container");
+    let buttons = container.getElementsByTagName("img");
+    for (let i=0; i<18; i++)
     {
-        var buttons_opc = document.getElementById("button"+i);
+        let buttons_opc = document.getElementById("button"+i);
         buttons[i].src = "images/hover_button.png";
         buttons_opc.style.opacity = "";
     }
 }
 
-function exportPng() {
+function exportPng()
+{
+
     domtoimage.toBlob(document.getElementById('container'))
         .then(function (blob) {
-            window.saveAs(blob, 'fretboard.png');
+            window.saveAs(blob, 'chord_box.png');
         })
-}
-
-function drawAgain() {
-    var container = document.getElementById("container");
-    var button_pos = container.getElementsByTagName("img");
-    var button_index = [];
-    for(let i=0; i<24; i++)
-    {
-        if (button_pos[i].src.match('images/button1.png'))
-        button_index.push(i);
-
-        if (button_pos[i].src.match('images/button2.png'))
-        button_index.push(i);
-
-        if (button_pos[i].src.match('images/button3.png'))
-        button_index.push(i);
-
-        if (button_pos[i].src.match('images/button4.png'))
-        button_index.push(i);
-    }
-    for(var element of button_index)
-        console.log(element);
-
 }
